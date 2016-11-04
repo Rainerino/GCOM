9944a31fd40c2fcf5ac1e8e53cfc21eaf9fe3e8c
Benjamin Lang <benjaminlang12@gmail.com>
2016-11-03 19:16:42 -0700

    Added design notes

--- modules/uas_dcnc/dcnc.cpp
+++ modules/uas_dcnc/dcnc.cpp
@@ -12,6 +12,7 @@ DCNC::DCNC()
 
 DCNC::~DCNC()
 {
+    //close the clientSocket if it is open check if the ptr is null
     server->close();
 }
 
@@ -27,22 +28,25 @@ void DCNC::startServer(int port, std::string addr)
 void DCNC::handleConection()
 {
     if(isConnected){
-        std::cout << "we are already connected" << std::endl;
+        QDebug("we are already connected \n");
         return;
     }
     //otherwise let's accept a new connection
     isConnected = true;
     clientConnection = server->nextPendingConnection();
+    
+    //replace this garbage with the QTCPSocket signal to trigger handle Data as a socket
     while(clientConnection->bytesAvailable() <= 0){}
     //do appropriate opperations with message
     handleData(clientConnection);
     clientConnection->close();
 }
 
+//should be a slot that responds to QTCPSignal
 void DCNC::handleData(QTcpSocket* socket)
 {
     //initialize new gremlin connection and prepare message
-    dataIn.setDevice(socket);
+    dataIn.setDevice(socket);               //should be done at the handle connection level
     std::unique_ptr<UASMessage> message;
     UASMessageTCPFramer messageFramer;
 
@@ -50,8 +54,8 @@ void DCNC::handleData(QTcpSocket* socket)
     while(!messageFramer.status()){
         dataIn.startTransaction();
         dataIn >> messageFramer;
+        //check the status of dataIn and the framer
         message = messageFramer.generateMessage();
-        messageFramer.frameMessage(*message.get());
         if(!messageFramer.status()){
             dataIn.rollbackTransaction();
             messageFramer.clearMessage();
@@ -63,6 +67,21 @@ void DCNC::handleData(QTcpSocket* socket)
     switch(message.MessageID){
         case //enumerate and emit right signals
     }*/
-
 }
+
+//create a slot to handle dropped connection - set is connected to false
+//check ID message if client is dropped flag
+//1. send a signal if a client is reconnection from dropout
+//2. send client a message to acknowledge that client dropped before
+
+//create new connection signal for UI if you get a new connection after a drop
+//create connection dropped signal
+
+//create method to force drop connection
+//if i get a dirty message tell the gremlin to reset => gaurantees always get an ID message after it
+//otherwise its a new connection and treat normally
+//
+
+
+
 
