aeb38e59679c54ab37f137b8de0fe78711bb4679
Zeyad Tamimi <ZeyadTamimi@outlook.com>
2016-11-27 15:54:45 -0700

    Fixed memory leak in setup & moved timeou to start
    - Setup would always create a new TCP Socket regardless
      of whether or not one existed previously, which would
      cause a memory leak
    - Moved timeout from setup to start and removed
    - added doxygen comments

--- modules/mavlink_relay/mavlink_relay_tcp.hpp
+++ modules/mavlink_relay/mavlink_relay_tcp.hpp
@@ -14,27 +14,57 @@
 // GCOM Includes
 #include "../Mavlink/ardupilotmega/mavlink.h"
 
-
 //===================================================================
 // Class Declarations
 //===================================================================
+/*!
+ * \brief The MAVLinkRelay class is designed to connect to Mission Planner, or
+ *        any other TCP mavlink messagesource and relay the relevant messages to
+ *        the rest of the program using relevant signals
+ * \author Grant Nicol
+ * \author Zeyad Tamimi
+ */
 class MAVLinkRelay : QObject
 {
     Q_OBJECT
+
 public:
+
     // Public Methods
     MAVLinkRelay();
 
-    bool start();
-    void setup(QString ipaddress, qint16 port, int timeout = 1000);
+    /*!
+     * \brief setup creates the TCP socket with all the specified settings and
+     *        connects all the internal slots. Multiple calls to setup will result
+     *        in stop being called followed by setup
+     * \param [in] ipAddress, the IP address of the TCP MAVLink stream
+     * \param [in] port, the port that the TCP MAVLink stream is broadcasting on
+     */
+    void setup(QString ipAddress, qint16 port);
+
+    /*!
+     * \brief start, attempts to open the TCP socket and connect to the MAVLink
+     *        stream
+     * \return True if the connection was successful. False if the operation
+     *         timed out or setup was never called
+     */
+    bool start(int timeout);
+
+    /*!
+     * \brief stop, tearsdown the TCP socket and disconnects the slots and
+     *        signals
+     * \return True if the TCP socket was torn down and False if stop was called
+     *         before start
+     */
     bool stop();
+
 signals:
     void mavrelayConnected();
     void mavrelayDisconnected();
     void mavrelayGPSInfo(std::shared_ptr<mavlink_global_position_int_t> gpsSignal);
     void mavrelayCameraInfo(std::shared_ptr<mavlink_camera_feedback_t> cameraSignal);
 
-public slots:
+private slots:
     void connected();
     void disconnected();
     void readBytes();
@@ -45,7 +75,6 @@ private:
     QTcpSocket *missionplannerSocket;
     QString ipaddress;
     qint16 port;
-    int timeout;
     mavlink_status_t lastStatus;
 };
 #endif // MAVLINKRELAYTCP_HPP
