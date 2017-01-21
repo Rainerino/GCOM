130cb2d5ff5f8a0320fb9f8d3f3b1258d8c8fd24
Jean <jrsyjueco@gmail.com>
2017-01-21 11:59:44 -0800

    Fixed build errors and update constructors

--- modules/uas_imgtag/image_tagger.cpp
+++ modules/uas_imgtag/image_tagger.cpp
@@ -3,29 +3,27 @@
 //===================================================================
 // System Includes
 #include <vector>
+#include <algorithm>
+#include <iterator>
 // GCOM Includes
 #include "image_tagger.hpp"
 #include "modules/uas_message/uas_message.hpp"
-
-//===================================================================
-// Constants
-//===================================================================
-const int SEQ_NUM_INDEX = 0;
-const int IMAGE_DATA_INDEX = 1;
 
 //===================================================================
 // Class Definitions
 //===================================================================
 ImageTagger::ImageTagger(unsigned char seqNum, unsigned char imageData[])
 {
-    this.seqNum = seqNum;
-    this.imageData = imageData;
+    this->seqNum = seqNum;
+
+    int len = sizeof(imageData) / sizeof(unsigned char);
+    std::copy(imageData, imageData + len, this->imageData.begin());
 }
 
 ImageTagger::ImageTagger(const std::vector<unsigned char> &serializedMessage)
 {
-    seqNum = serializedMessage[SEQ_NUM_INDEX];
-    // TODO: Set imageData to the rest of the data in serializedMessage
+    this->seqNum = static_cast<unsigned char>(serializedMessage.front());
+    std::copy(serializedMessage.begin() + 1, serializedMessage.end(), back_inserter(this->imageData));
 }
 
 ImageTagger::~ImageTagger()
@@ -43,6 +41,6 @@ std::vector<unsigned char> ImageTagger::serialize()
     std::vector<unsigned char> serializedMessage;
     serializedMessage.push_back(seqNum);
     serializedMessage.insert(std::end(serializedMessage),
-                             std::begin(imageData), std::end(imageData)); // ending included?
+                             std::begin(imageData), std::end(imageData));
     return serializedMessage;
 }
